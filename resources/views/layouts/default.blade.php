21bd8cff45e360da88229c13e010519f643af33b
domiee13 <dungntt1309@gmail.com>
2021-09-27 08:12:16 +0700

    Fix route

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -144,7 +144,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="/users" class="nav-link text-white {{ Request::path() ==  'users' ? 'active' : ''  }}">
+                    <a href="home/users" class="nav-link text-white {{ Request::path() ==  'users' ? 'active' : ''  }}">
                         <svg class="bi me-2" width="16" height="16">
                             <use xlink:href="#people-circle" />
                         </svg>
